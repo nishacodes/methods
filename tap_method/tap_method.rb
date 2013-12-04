eb7a904a62c5f7c452acdacb73c5ee77b572ae2e
nishacodes <nishabatra27@gmail.com>
2013-12-03 23:37:11 -0500

    write my own tap method

---
+++ tap_method/tap_method.rb
@@ -0,0 +1,26 @@
+class Object
+	def my_tap
+		yield self
+		self
+	end
+end
+
+# # EXAMPLE
+# # Keep my_tap method within the class to use it in the class
+# # -----------------------------------------------------------
+
+# class Person 
+# 	attr_accessor :name, :age
+
+# 	def initialize
+# 		@name
+# 		@age
+# 	end
+# end
+
+# Person.new.my_tap do |person| 
+# 	person.name = "Nisha" 
+# 	person.age = 28
+# end
+
+# # => returns an object of Person w/name and age assigned
